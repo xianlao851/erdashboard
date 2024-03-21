9c6f1fc111445ca9ac41b68c17225a5317b7f0d5
xianlao851 <xianlao851@gmail.com>
2024-03-21 13:39:08 +0800

    add equal sign 2

--- app/Http/Livewire/Dash/SecondMonitor.php
+++ app/Http/Livewire/Dash/SecondMonitor.php
@@ -461,7 +461,7 @@ class SecondMonitor extends Component
             if ($this->erAdmittedCount < floor($erslot * .5)) {
                 $this->erAdmittedCountColor = '#04bd55';
             }
-            if ($this->erAdmittedCount > floor($erslot * .5)) {
+            if ($this->erAdmittedCount >= floor($erslot * .5)) {
                 $this->erAdmittedCountColor = '#bd4602';
             }
             if ($this->erAdmittedCount >= floor($erslot * .8)) {
